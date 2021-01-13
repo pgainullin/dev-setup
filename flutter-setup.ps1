8e41162337bd8860fba21c9245cf89f8bdbfe05c
pgainullin <pgainullin@gmail.com>
2021-01-13 09:12:37 +0800

    hotfix for non-server setup

--- flutter-setup.ps1
+++ flutter-setup.ps1
@@ -1,4 +1,3 @@
-
 [String] $customPath = $args[0]
 
 Write-Warning  "Windows Development Machine Set-up Script for Android/Flutter" 
@@ -9,11 +8,11 @@ Write-Host -NoNewline "Checking Execution Policy... "
 Write-Host $outStr
 
 
-if($null -eq $customPath){
-    Write-Warning  "Using system default path" 
-} else {
-    Write-Warning  "Using custom path ${customPath} (UNIMPLEMENTED)" 
-}
+#if($null -eq $customPath){
+#    Write-Warning  "Using system default path" 
+#} else {
+#    Write-Warning  "Using custom path ${customPath} (UNIMPLEMENTED)" 
+#}
  
 #function to update system path after installation 
 function Update-Environment-Path
@@ -25,67 +24,68 @@ function Update-Environment-Path
 }
  
 Set-ExecutionPolicy Bypass -Scope Process -Force;
-$ifChocoInstalled = powershell choco -v
+#$ifChocoInstalled = powershell choco -v
  
  
 #Choco installation
-if(-not($ifChocoInstalled)){
-    Write-host "Chocolatey is not installed, installing now " 
-    Set-ExecutionPolicy Bypass -Scope Process -Force;
+#if(-not($ifChocoInstalled)){
+#    Write-host "Chocolatey is not installed, installing now " 
     Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1') )
  
     Update-Environment-Path
  
-} else{
-    Write-host "Chocolatey $ifChocoInstalled is already installed" 
-}
+#} else{
+#    Write-host "Chocolatey $ifChocoInstalled is already installed" 
+#}
  
 
 
 #GIT Installation
  
-$ifGITInstalled = powershell git --version
+#$ifGITInstalled = powershell git --version
  
  
-if(-not($ifGITInstalled)){
-    Write-host "GIT is not installed, installing now " 
-    Set-ExecutionPolicy Bypass -Scope Process -Force;
-    choco install git --yes
+#if(-not($ifGITInstalled)){
+#    Write-host "GIT is not installed, installing now " 
+    echo A | choco install git --yes
   #  choco install gitextensions
      
     Update-Environment-Path
  
-} else {
-    Write-host "$ifGITInstalled is already installed" 
-}
+#} else {
+#    Write-host "$ifGITInstalled is already installed" 
+#}
  
 
 #7-zip 
-#choco install 7zip.portable
+choco install 7zip.portable
+Update-Environment-Path
 
 #Notepad++ 
-choco install notepadplusplus.install
+echo A | choco install notepadplusplus.install
 Update-Environment-Path
  
 #Flutter, Chrome and Android Studio
-choco install flutter
+echo A | choco install android-sdk
 Update-Environment-Path
-choco install googlechrome
+echo A | choco install flutter
 Update-Environment-Path
-choco install androidstudio
+echo A | choco install googlechrome
+Update-Environment-Path
+echo A | choco install androidstudio
 Update-Environment-Path
 
 #ADB
-choco install adb
+echo A | choco install adb
 
 #VSCode
-choco install vscode
+echo A | choco install vscode
 
 #Accept Android SDK Licences
 Write-host "Accepting Android Licences"
 #Convert $dir to its short name equivalent
 $dir=(New-Object -com scripting.filesystemobject).getFolder("$env:ANDROID_SDK_ROOT\tools\bin").ShortPath
-Invoke-Expression("echo yes | $dir\sdkmanager.bat --licenses")
+Invoke-Expression("echo y | $dir\sdkmanager.bat --licenses")
 # Invoke-Expression("$dir\sdkmanager.bat --licenses")
 
 #setup Flutter
